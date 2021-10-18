34081c109b54c0e47c64fe53bb1b86fe78f6623c
Muhammed shafaf <shafafmuhammad6@gmail.com>
2021-10-18 19:18:53 +0530

    added security

--- index.php
+++ index.php
@@ -1,79 +1,102 @@
-
- <?php
- /*
-  * Plugin Name: Personal Plugin demo
-  * Plugin URI: https://www.google.com
-  * Description: This is sample plugin
+<?php
+ /**
+  * Plugin Name: Meta box Plugin.
+  * Plugin URI: https://www.example.com
+  * Description: meta box plugin for text field.
   * Version: 0.1
   * Author: Muhammed Shafaf
-  * Author URI: https://www.facebook.com
+  * Author URI: https://muhammad-shafaf123.github.io/personal/
   */
 
-/* root metabox class */
-class MetaBoxClass{
-  /* constucter call all action hooks */
-  function __construct(){
-     add_action('add_meta_boxes',array($this,'text_field_meta_box'));
-     add_action( 'save_post', array($this,'save_post_data'));
-     add_action( 'the_content',array($this, 'filter_the_content'));
-  }
+/**
+* Exit if accessed directly
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+      exit;
+}
+/**
+*  root metabox class
+*/
+if ( !class_exists( 'MetaBoxClass' ) ) {
+  class MetaBoxClass {
+    /**
+    * constucter call all action hooks
+    */
+    function __construct() {
+      add_action( 'add_meta_boxes',array( $this, 'text_field_meta_box' ) );
+      add_action( 'save_post', array( $this, 'save_post_data' ) );
+      add_filter( 'the_content',array( $this, 'filter_the_content' ) );
+    }
+    public function text_field_meta_box() {
+      add_meta_box( 'radio-1', 'Custom Message Box', array( $this, 'display_callback' ),'post' );
+    }
+    /**
+    * meta box callback function.
+    */
+    public function display_callback( $object ) {
+      ?>
+      <form name="form" action="" method="POST">
+        <label for="author name">Author</label>
+        <input id="meta_key" type="text" name="meta_text"
+            value="<?php $field_value= get_post_meta ( $object->ID, "meta_key",true ); echo $field_value; ?>">
+        <label class="label-show" for="show the content">Show</label>
+        <input type="checkbox"  name="checked" value="true" >
+        <?php wp_nonce_field( 'nonce_action', 'nonce_field' ); ?>
+      </form>
+      <?php
+    }
 
-  public function text_field_meta_box(){
-     add_meta_box('radio-1','Custom Message Box',array($this,'display_callback'),'post');
-  }
-
- /* function for creating ul design. */
-  public function display_callback($object){
-     ?>
-     <form name="form" action="" method="POST">
-     <label for="author name">Author</label>
-     <input id="meta_key" type="text" name="meta_text"
-     value="<?php echo get_post_meta ($object->ID, "meta_box_key",true); ?>">
-     <label class="label-show" for="show the content">Show</label>
-     <input type="checkbox"  name="checked" value="true" >
-     <?php wp_nonce_field( 'nonce_action', 'nonce_field' ); ?>
-     </form>
-     <?php
-     get_post_meta ($object->ID, "meta_box_key",true);
-  }
-
-  public function filter_the_content($content) {
-     global $post;
-		 /* retrieve the metadata values if they exist */
-		 $data_field = get_post_meta($post -> ID, 'meta_key', true);
-     $data_checkbox = get_post_meta($post -> ID, 'check_box_meta_key', true);
-		 if (!empty($data_field) && $data_checkbox == "true") {
-		     $custom_message = "<div style='font-weight:bold;text-align:center'>";
-			   $custom_message .= $data_field;
-			   $custom_message .= "</div>";
-			   $content = $custom_message . $content;
-		 }
-
-		return $content;
-  }
-
- /* add the field value to database */
-  public function save_post_data($post_id) {
-    /* check nonce */
-    if (! isset( $_POST['nonce_field'] ) || ! wp_verify_nonce( $_POST['nonce_field'], 'nonce_action' )) {
-       wp_nonce_ays( 'Invalid user' );
-    } else {
-      if (isset($_POST['checked'])) {
-          update_post_meta($post_id,'check_box_meta_key',$_POST['checked']);
-      }else {
-          update_post_meta($post_id,'check_box_meta_key',$_POST['checked']);
+    public function filter_the_content( $content ) {
+      global $post;
+      /**
+      * retrieve the metadata values if they exist
+      */
+      $data_field = get_post_meta( $post -> ID, 'meta_key', true );
+      $data_checkbox = get_post_meta( $post -> ID, 'check_box_meta_key', true );
+      if ( !empty( $data_field ) && $data_checkbox == "true" ) {
+        $custom_message = "<div style='font-weight:bold;text-align:center'>";
+        $custom_message .= $data_field;
+        $custom_message .= "</div>";
+        $content = $custom_message . $content;
       }
-
-      if ( array_key_exists( 'meta_text', $_POST ) ) {
-          /* sanitize text field */
-          $data = sanitize_text_field($_POST['meta_text']);
-          update_post_meta($post_id,'meta_key',$data);
+        return $content;
+    }
+    /**
+    * add the field value to database
+    */
+    public function save_post_data( $post_id ) {
+      /**
+      * check nonce
+      */
+      if ( !isset( $_POST['nonce_field'] ) || ! wp_verify_nonce( $_POST['nonce_field'], 'nonce_action' ) ) {
+        return $post_id;
+      }else {
+        /**
+        * check it is a valid user.
+        */
+        if ( !current_user_can( 'edit_post', $post_id ) ) {
+          return $post_id;
+        }
+        if ( isset( $_POST['checked']) ) {
+          /**
+          * sanitize check book field
+          */
+          $data_checkbox_field = sanitize_text_field( $_POST['checked'] );
+          update_post_meta( $post_id,'check_box_meta_key',$data_checkbox_field );
+        }
+        if( isset( $_POST['meta_text'] ) ) {
+          /**
+          * sanitize text field
+          */
+          $data_text_field = sanitize_text_field( $_POST['meta_text'] );
+          update_post_meta( $post_id,'meta_key',$data_text_field );
+        }
       }
     }
   }
-
+}else {
+    exit();
 }
 
 new  MetaBoxClass;
-
 ?>
--- wp_shortcode.php
+++ wp_shortcode.php
@@ -1,27 +1,42 @@
-
- <?php
- /*
-  * Plugin Name: Personal Plugin demo
-  * Plugin URI: https://www.google.com
-  * Description: This is sample plugin
+<?php
+ /**
+  * Plugin Name: shortcode Plugin demo
+  * Plugin URI: https://www.example.com
+  * Description: plugin for printing text, and you can change the color of the text, use [write color=blue].
   * Version: 0.1
   * Author: Muhammed Shafaf
-  * Author URI: https://www.facebook.com
+  * Author URI: https://muhammad-shafaf123.github.io/personal/
   */
-  class WP_class{
-    public function __construct(){
-      add_shortcode('write',array($this, 'print_text'));
 
-    }
-    function print_text($atts){
-      $changes = shortcode_atts(array(
-        'color' => "#000"
-      ),$atts);
-      $print_name = '<h2 style= "color:'.$changes['color'].';">Some text</h2>';
-      return $print_name;
-    }
-  }
+/**
+* Exit if accessed directly
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+   exit;
+}
+/**
+* Check that the class exists before trying to use it
+*/
+if ( !class_exists( 'WpTextShortcode' ) ) {
+   class WpTextShortcode{
+       public function __construct() {
+           add_shortcode( 'write',array( $this, 'print_text_shortcode_callback' ) );
+       }
+       /**
+       * shortcode callback function.
+       */
+       function print_text_shortcode_callback( $atts ) {
+           $changes = shortcode_atts( array(
+              'color' => "#000"
+           ), $atts );
+           $print_name = '<h2 style= "color:'.$changes['color'].';">Some text</h2>';
+           return $print_name;
+       }
+   }
+}else {
+    exit;
+}
 
-  $textPrint = new WP_class();
+new WpTextShortcode();
 
 ?>
